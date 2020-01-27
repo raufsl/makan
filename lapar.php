df142f4e1aaea2730e259e8fdb5a36705eccf6b4
raufsl <57763124+raufsl@users.noreply.github.com>
2020-01-27 22:14:46 +0700

    Update lapar.php

--- lapar.php
+++ lapar.php
@@ -31,7 +31,7 @@ function change(){
         $pilihan = trim(fgets(STDIN));
         if($pilihan == "y" || $pilihan == "Y"){
         echo color("red","===========(REDEEM VOUCHER)===========");
-        echo "\n".color("yellow","!] Claim voc PAKEGOJEK");
+        echo "\n".color("yellow","!] Claim voc GOFOODYUK");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
@@ -44,7 +44,7 @@ function change(){
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$message);
-        echo "\n".color("yellow","!] Claim voc GOFOODYUK");
+        echo "\n".color("yellow","!] Claim voc PESENGOFOOD");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
@@ -58,7 +58,7 @@ function change(){
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$messageboba10);
-        echo "\n".color("yellow","!] Claim voc GOFOODBOBA19");
+        echo "\n".color("yellow","!] Claim voc AYOGOJEK");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
